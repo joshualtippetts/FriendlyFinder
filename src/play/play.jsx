cd6172f223a4f5e12b1fed7bd6373833da8afe6a
joshua <jlt2005@byu.edu>
2026-02-07 19:59:33 -0700

    play.css and filled in play.jsx

--- src/play/play.jsx
+++ src/play/play.jsx
@@ -1,9 +1,45 @@
 import React from 'react';
+import './play.css';
 
 export function Play() {
   return (
     <main>
-      <div>play displayed here</div>
+        <section>
+            <h3 className="subtitle">Player Profile: <span>You</span></h3> 
+
+            <div id="table">
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+            </div>
+        </section>
     </main>
   );
 }
