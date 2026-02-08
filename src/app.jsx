2f1e1b590c424cb042eb28fda71dc9e1598bef08
joshua <jlt2005@byu.edu>
2026-02-07 20:13:55 -0700

    Fix script placement for bootstrap

--- src/app.jsx
+++ src/app.jsx
@@ -33,21 +33,22 @@ export default function App() {
                     </nav>
                 </header>
 
-        <Routes>
-            <Route path='/' element={<Login />} exact />
-            <Route path='/play' element={<Play />} />
-            <Route path='/leaderboard' element={<Leaderboard />} />
-            <Route path='*' element={<NotFound />} />
-        </Routes>
+            <Routes>
+                <Route path='/' element={<Login />} exact />
+                <Route path='/play' element={<Play />} />
+                <Route path='/leaderboard' element={<Leaderboard />} />
+                <Route path='*' element={<NotFound />} />
+            </Routes>
 
-        <footer>
-            <div className="author">
-            <span>Joshua Tippetts</span>
-            <a className="repo" href="https://github.com/joshualtippetts/Word-duh-le">
-                Github
-            </a>
-            </div>
-        </footer>
+            <footer>
+                <div className="author">
+                <span>Joshua Tippetts</span>
+                <a className="repo" href="https://github.com/joshualtippetts/Word-duh-le">
+                    Github
+                </a>
+                </div>
+            </footer>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
         </div>
     </BrowserRouter>
   );
