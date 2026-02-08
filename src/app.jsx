6518e127d110149c0ddc184e0f904b122df5dff8
joshua <jlt2005@byu.edu>
2026-02-07 21:00:26 -0700

    Fixing image problems and bootstrap stuff

--- src/app.jsx
+++ src/app.jsx
@@ -12,7 +12,7 @@ export default function App() {
             <div className="body">
                 <header>
                     <nav className="nav-body">
-                        <img id="logo-head" src="./public/word-duh-le_logo.png" alt="Word-duh-le logo"/>
+                        <img id="logo-head" src="/word-duh-le_logo.png" alt="Word-duh-le logo"/>
                         <menu className="nav nav-pills">
                             <li className="nav-item">
                                 <NavLink className="nav-link" to="">
@@ -48,7 +48,6 @@ export default function App() {
                 </a>
                 </div>
             </footer>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
         </div>
     </BrowserRouter>
   );
