3f16db9e8e6ae79f0295f41bcc4db50bca8c226b
joshua <jlt2005@byu.edu>
2026-02-07 20:10:32 -0700

    leaderboard.css and filled leaderboard.jsx

--- src/leaderboard/leaderboard.jsx
+++ src/leaderboard/leaderboard.jsx
@@ -1,9 +1,49 @@
 import React from 'react';
+import './leaderboard.css';
 
 export function Leaderboard() {
   return (
     <main>
-      <div>leaderboard displayed here</div>
+        <h3 className="subtitle">Daily Leaderboard</h3>
+        <table>
+            <thead>
+                <th className="table-placement">#</th>
+                <th className="table-player">Player</th>
+                <th className="table-tries">Tries</th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td className="table-placement">1</td>
+                    <td className="table-player">John</td>
+                    <td className="table-tries">2</td>
+                </tr>
+                <tr>
+                    <td className="table-placement">2</td>
+                    <td className="table-player">Jane</td>
+                    <td className="table-tries">3</td>
+                </tr>
+                <tr>
+                    <td className="table-placement">3</td>
+                    <td className="table-player">You</td>
+                    <td className="table-tries">4</td>
+                </tr>
+            </tbody>
+        </table>
+
+        <table id="your-stats">
+            <thead>
+                <th className="table-placement">#</th>
+                <th className="table-player">Player</th>
+                <th className="table-tries">Tries</th>
+            </thead>
+            <tbody>
+                <tr>
+                    <td className="table-placement">3</td>
+                    <td className="table-player">You</td>
+                    <td className="table-tries">4</td>
+                </tr>
+            </tbody>
+        </table>
     </main>
   );
 }
