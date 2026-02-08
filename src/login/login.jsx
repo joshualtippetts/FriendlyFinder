53bbb3be02967bf66c087469e5d9cfa735bae0e8
joshua <jlt2005@byu.edu>
2026-02-07 19:54:58 -0700

    login.css and filled in login.jsx

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import './login.css';
 
 export function Login() {
   return (
     <main>
-      <div>login displayed here</div>
+        <div id="title">
+            <h1>Welcome to Word-duh-le!</h1>
+            <h2>Login to play!</h2>
+        </div>
+        <form method="get" action="play.html">
+            <div id="login-input">
+                <div id="username">
+                    <label id="name-label">Username:</label>
+                    <input type="text" placeholder="Username"/>
+                </div>
+                <div id="password">
+                    <label id="password-label">Password:</label>
+                    <input type="password" placeholder="Password"/>
+                </div>
+            </div>
+            <div id="login-confirm">
+                <button type="submit">Create</button>
+                <button type="submit">Confirm</button>
+            </div>
+        </form>
+
+        <div id="quote-body">
+            <h5>Quote of the day: <span id="quote">"Quit, don't quit. Noodles, don't noodles."</span></h5>
+        </div>
     </main>
   );
 }
