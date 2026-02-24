d5fafd6189b7913ac0caddeff9f71d233acc7c00
joshua <jlt2005@byu.edu>
2026-02-23 18:58:49 -0700

    Uniformity

--- src/app.jsx
+++ src/app.jsx
@@ -9,70 +9,70 @@ import { AuthState } from './login/authState';
 
 
 export default function App() {
-    const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
-    const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
-    const [authState, setAuthState] = React.useState(currentAuthState);
+  const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
+  const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
+  const [authState, setAuthState] = React.useState(currentAuthState);
 
 
-    return (
-        <BrowserRouter>
-            <div className="body">
-                <header>
-                    <nav className="nav-body">
-                        <img id="logo-head" src="/word-duh-le_logo.png" alt="Word-duh-le logo"/>
-                        <menu className="nav nav-pills">
-                            <li className="nav-item">
-                                <NavLink className="nav-link" to="">
-                                    Login
-                                </NavLink>
-                            </li>
-                            {authState === AuthState.Authenticated && (
-                                <li className="nav-item">
-                                    <NavLink className="nav-link" to="play">
-                                        Play
-                                    </NavLink>
-                                </li>
-                            )}
-                            {authState === AuthState.Authenticated && (
-                                <li className="nav-item">
-                                    <NavLink className="nav-link" to="leaderboard">
-                                        Leaderboard
-                                    </NavLink>
-                                </li>
-                            )}
-                        </menu>
-                    </nav>
-                </header>
+  return (
+    <BrowserRouter>
+      <div className="body">
+        <header>
+          <nav className="nav-body">
+            <img id="logo-head" src="/word-duh-le_logo.png" alt="Word-duh-le logo"/>
+            <menu className="nav nav-pills">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="">
+                  Login
+                </NavLink>
+              </li>
+              {authState === AuthState.Authenticated && (
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="play">
+                    Play
+                  </NavLink>
+                </li>
+              )}
+              {authState === AuthState.Authenticated && (
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="leaderboard">
+                    Leaderboard
+                  </NavLink>
+                </li>
+              )}
+            </menu>
+          </nav>
+        </header>
 
-            <Routes>
-                <Route
-                    path='/'
-                    element={
-                        <Login
-                            userName={userName}
-                            authState={authState}
-                            onAuthChange={(userName, authState) => {
-                            setAuthState(authState);
-                            setUserName(userName);
-                            }}
-                        />
-                    }
-                    exact
-                />
-                <Route path='/play' element={<Play />} />
-                <Route path='/leaderboard' element={<Leaderboard />} />
-                <Route path='*' element={<NotFound />} />
-            </Routes>
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <Login
+                userName={userName}
+                authState={authState}
+                onAuthChange={(userName, authState) => {
+                setAuthState(authState);
+                setUserName(userName);
+                  }}
+              />
+            }
+              exact
+          />
+          <Route path='/play' element={<Play />} />
+          <Route path='/leaderboard' element={<Leaderboard />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
 
-            <footer>
-                <div className="author">
-                <span>Joshua Tippetts</span>
-                <a className="repo" href="https://github.com/joshualtippetts/Word-duh-le">
-                    Github
-                </a>
-                </div>
-            </footer>
-        </div>
+        <footer>
+          <div className="author">
+            <span>Joshua Tippetts</span>
+            <a className="repo" href="https://github.com/joshualtippetts/Word-duh-le">
+              Github
+            </a>
+          </div>
+        </footer>
+      </div>
     </BrowserRouter>
   );
 }
