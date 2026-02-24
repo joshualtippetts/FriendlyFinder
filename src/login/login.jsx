b85916022cedcca7371587572a89285007f85787
joshua <jlt2005@byu.edu>
2026-02-23 18:53:18 -0700

    Uniformity

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -8,42 +8,26 @@ import { AuthState } from './authState';
 export function Login({ userName, authState, onAuthChange }) {
   return (
     <main>
-        <div id="title">
-            <h1>Welcome to Word-duh-le!</h1>
-        </div>
-        <div>
-            {authState === AuthState.Authenticated && (
-            <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
-            )}
-            {authState === AuthState.Unauthenticated && (
-            <Unauthenticated
-                userName={userName}
-                onLogin={(loginUserName) => {
-                onAuthChange(loginUserName, AuthState.Authenticated);
-                }}
-            />
-            )}
-        </div>
-        {/* <form method="get" action="play.html">
-            <div id="login-input">
-                <div id="username">
-                    <label id="name-label">Username:</label>
-                    <input type="text" placeholder="Username"/>
-                </div>
-                <div id="password">
-                    <label id="password-label">Password:</label>
-                    <input type="password" placeholder="Password"/>
-                </div>
-            </div>
-            <div id="login-confirm">
-                <button type="submit">Create</button>
-                <button type="submit">Confirm</button>
-            </div>
-        </form> */}
+      <div id="title">
+        <h1>Welcome to Word-duh-le!</h1>
+      </div>
+      <div>
+        {authState === AuthState.Authenticated && (
+        <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
+        )}
+        {authState === AuthState.Unauthenticated && (
+        <Unauthenticated
+          userName={userName}
+          onLogin={(loginUserName) => {
+          onAuthChange(loginUserName, AuthState.Authenticated);
+          }}
+        />
+        )}
+      </div>
 
-        <div id="quote-body">
-            <h5>Quote of the day: <span id="quote">"Quit, don't quit. Noodles, don't noodles."</span></h5>
-        </div>
+      <div id="quote-body">
+        <h5>Quote of the day: <span id="quote">"Quit, don't quit. Noodles, don't noodles."</span></h5>
+      </div>
     </main>
   );
 }
